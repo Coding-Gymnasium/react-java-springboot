8d9f60b3aedaa8686a8e2ff39fe3f67f5cea6af1
Nico Rithner <nicorithner@gmail.com>
2024-07-07 16:34:48 -0600

    Add onClick function to Add todos

--- react-todos/src/App.js
+++ react-todos/src/App.js
@@ -8,12 +8,26 @@ function App() {
     { rowNumber: 3, rowDescription: "Make dinner", rowAssigned: "User One" },
   ];
 
+  const addTodo = () => {
+    if (todos.length > 0) {
+      const newTodo = {
+        rowNumber: todos.length + 1,
+        rowDescription: "New todo",
+        rowAssigned: "User Three",
+      };
+      todos.push(newTodo);
+      return <TodoTable todos={todos}/>
+    }
+  };
+
   return (
     <div className="mt-5 container">
       <div className="card">
         <div className="card-header">Your Todo's</div>
         <div className="card-body">
-              <TodoTable todos = {todos} />
+          <button className="btn btn-primary" onClick={addTodo}>
+            Add New To-Do
+          </button>
         </div>
       </div>
     </div>
